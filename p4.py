0c63e4eb7cd921478f46b6fe3e632e390983b4e1
Alex Pepper <axpepper@gmail.com>
2018-07-27 03:09:53 -0400

    actually took average

--- p4.py
+++ p4.py
@@ -5,6 +5,7 @@ import sys
 
 def convert_bw(matrix):#takes an image ndarry with shape (height, width, 3), dtype
     total = matrix[:,:,0] + matrix[:,:,1] + matrix[:,:,2]
+    total = total/3
     return np.fromfunction(lambda x,y,z: total[x,y] , matrix.shape(), dtype=np.uint )
     #np.uint8
     #returns
