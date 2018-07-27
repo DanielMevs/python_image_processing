22c820c03a4e938b80e1b820d4aea605ef9a30e9
Alex Pepper <axpepper@gmail.com>
2018-07-26 23:41:57 -0400

    converts to black and white

--- p4.py
+++ p4.py
@@ -1,5 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import sys
 
-def convert_bw():#takes an image ndarry with shape (height, width, 3), dtype
+def convert_bw(matrix):#takes an image ndarry with shape (height, width, 3), dtype
+    total = matrix[:,:,0] + matrix[:,:,1] + matrix[:,:,2]
+    return np.fromfunction(lambda x,y,z: total[x,y] , matrix.shape(), dtype=np.uint )
     #np.uint8
     #returns
